aefd2e99b1271168357bb8c59e2f274a07b867d7
Robinnsson Cifuentes Ramírez <robinnsson@gmail.com>
2021-06-28 21:23:15 -0500

    Optimized DAO

--- app/src/main/java/com/c2r/peliculasapp/data/local/MovieDao.kt
+++ app/src/main/java/com/c2r/peliculasapp/data/local/MovieDao.kt
@@ -5,7 +5,6 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.c2r.peliculasapp.data.model.MovieEntity
-import com.c2r.peliculasapp.data.model.MovieList
 
 @Dao
 interface MovieDao {
